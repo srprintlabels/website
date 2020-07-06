7efcc6a15811266542c4bd78cfb7f2686b3e101b
Joel Vinay Kumar <joel@auzmor.com>
2020-07-06 23:30:33 +0530

    more changes

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,46 +1,77 @@
-import React from 'react'
+import React from "react";
 
-import welcomeBanner from '../assets/img/bg-img/welcome-bg.png'
-import adhesive from '../assets/img/home-carousel/adhesive.png'
-import leaflets from '../assets/img/home-carousel/leaflets.png'
-import barcode from '../assets/img/home-carousel/barcode.png'
+import welcomeBanner from "../assets/img/bg-img/welcome-bg.png";
+import adhesive from "../assets/img/home-carousel/adhesive.png";
+import leaflets from "../assets/img/home-carousel/leaflets.png";
+import barcode from "../assets/img/home-carousel/barcode.png";
+import books from "../assets/img/home-carousel/books.png";
+import monocartons from "../assets/img/home-carousel/monocartons.png";
 
-export const Carousel = ()=> {
-  return <section 
-    className="welcome_area clearfix"
-    id="home" 
-    style={{
-      backgroundImage: `url(${welcomeBanner})`, 
-      padding:'10vh 0 0 0',
-      display: 'flex',
-      justifyContent: 'center'
-    }}
-  >    
-    <div id="carouselExampleIndicators" class="carousel slide landing-carousel" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src={adhesive} alt="First slide" />
+export const Carousel = () => {
+  return (
+    <section
+      className="welcome_area clearfix"
+      id="home"
+      style={{
+        backgroundImage: `url(${welcomeBanner})`,
+        padding: "10vh 0 0 0",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        id="carouselExampleIndicators"
+        class="carousel slide landing-carousel"
+        data-ride="carousel"
+      >
+        <ol class="carousel-indicators">
+          <li
+            data-target="#carouselExampleIndicators"
+            data-slide-to="0"
+            class="active"
+          ></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={adhesive} alt="First slide" />
+          </div>
+          <div class="carousel-item">
+            <img src={leaflets} alt="Second slide" />
+          </div>
+          <div class="carousel-item">
+            <img src={barcode} alt="Third slide" />
+          </div>
+          <div class="carousel-item">
+            <img src={books} alt="Fourth slide" />
+          </div>
+          <div class="carousel-item">
+            <img src={monocartons} alt="Third slide" />
+          </div>
         </div>
-        <div class="carousel-item">
-          <img src={leaflets} alt="Second slide" />
-        </div>
-        <div class="carousel-item">
-          <img src={barcode} alt="Third slide" />
-        </div>
+        <a
+          class="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a
+          class="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-  </section>
-}
+    </section>
+  );
+};
--- src/components/services/FoldingCartons.js
+++ src/components/services/FoldingCartons.js
@@ -1,67 +1,75 @@
-import React from 'react'
+import React from "react";
 
-import breadcrumb from '../../assets/img/core-img/breadcumb.png'
-import cartons1 from '../../assets/img/portfolio-img/cartons/1.png'
-import cartons2 from '../../assets/img/portfolio-img/cartons/2.png'
-import cartons3 from '../../assets/img/portfolio-img/cartons/3.png'
-import cartons4 from '../../assets/img/portfolio-img/cartons/4.png'
+import breadcrumb from "../../assets/img/core-img/breadcumb.png";
+import cartons1 from "../../assets/img/portfolio-img/cartons/1.png";
+import cartons2 from "../../assets/img/portfolio-img/cartons/2.png";
+import cartons3 from "../../assets/img/portfolio-img/cartons/3.png";
+import cartons4 from "../../assets/img/portfolio-img/cartons/4.png";
 
-import { Header, Footer } from '../../components'
+import { Header, Footer } from "../../components";
 
-export const FoldingCartons = (props)=> {
-  return <div>
-    <Header />
-    <div className="mosh-breadcumb-area" style={{backgroundImage: `url(${breadcrumb})`}}>
-      <div className="container h-100">
-        <div className="row h-100 align-items-center">
-          <div className="col-12">
-            <div className="bradcumbContent">
-              <h3>Folding Cartons</h3>
+export const FoldingCartons = (props) => {
+  return (
+    <div>
+      <Header />
+      <div
+        className="mosh-breadcumb-area"
+        style={{ backgroundImage: `url(${breadcrumb})` }}
+      >
+        <div className="container h-100">
+          <div className="row h-100 align-items-center">
+            <div className="col-12">
+              <div className="bradcumbContent">
+                <h3>Folding Cartons</h3>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    
-    <div className="gallery-content">
-      <div class="mosh-portfolio" style={{
-        gridTemplateColumns: 'auto auto auto auto'
-      }}>
-        <div class="single_gallery_item grid-item">
-          <img src={cartons1} alt="" />
-          <div class="gallery-hover-overlay d-flex align-items-center justify-content-center">
-            <div class="port-hover-text text-center">
-              <h4>Brochure</h4>
+
+      <div className="gallery-content">
+        <div
+          class="mosh-portfolio"
+          style={{
+            gridTemplateColumns: "auto auto",
+          }}
+        >
+          <div class="single_gallery_item grid-item">
+            <img src={cartons1} alt="" />
+            <div class="gallery-hover-overlay d-flex align-items-center justify-content-center">
+              <div class="port-hover-text text-center">
+                <h4>Brochure</h4>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="single_gallery_item grid-item">
-          <img src={cartons2} alt="" />
-          <div class="gallery-hover-overlay d-flex align-items-center justify-content-center">
-            <div class="port-hover-text text-center">
-              <h4>Brochure</h4>
+          <div class="single_gallery_item grid-item">
+            <img src={cartons2} alt="" />
+            <div class="gallery-hover-overlay d-flex align-items-center justify-content-center">
+              <div class="port-hover-text text-center">
+                <h4>Brochure</h4>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="single_gallery_item grid-item">
-          <img src={cartons3} alt="" />
-          <div class="gallery-hover-overlay d-flex align-items-center justify-content-center">
-            <div class="port-hover-text text-center">
-              <h4>Leaflet</h4>
+          <div class="single_gallery_item grid-item">
+            <img src={cartons3} alt="" />
+            <div class="gallery-hover-overlay d-flex align-items-center justify-content-center">
+              <div class="port-hover-text text-center">
+                <h4>Leaflet</h4>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="single_gallery_item grid-item">
-          <img src={cartons4} alt="" />
-          <div class="gallery-hover-overlay d-flex align-items-center justify-content-center">
-            <div class="port-hover-text text-center">
-              <h4>Catalogs</h4>
+          <div class="single_gallery_item grid-item">
+            <img src={cartons4} alt="" />
+            <div class="gallery-hover-overlay d-flex align-items-center justify-content-center">
+              <div class="port-hover-text text-center">
+                <h4>Catalogs</h4>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <Footer />
-  </div>
-}
+      <Footer />
+    </div>
+  );
+};
