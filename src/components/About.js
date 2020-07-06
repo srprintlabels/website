591d98d147a55524b14bc1a8cbf2b631c03bcf9e
Joel Vinay Kumar <joel@auzmor.com>
2020-07-07 00:36:44 +0530

    more changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,48 +1,72 @@
-import React from 'react';
+import React from "react";
 
-import breadcrumb from '../assets/img/core-img/breadcumb.png';
-import certificate from '../assets/img/home-carousel/certy.png'
+import breadcrumb from "../assets/img/core-img/breadcumb.png";
+import certificate from "../assets/img/home-carousel/certy.png";
 
-import { Header, Footer } from '../components'
+import { Header, Footer } from "../components";
 
-export const About = (props)=> {
-  return <div>
-    <Header />
-    <div className="mosh-breadcumb-area" style={{
-      backgroundImage: `url(${breadcrumb})`,
-      backgroundSize: '100vw',
-      backgroundRepeat: 'no-repeat',
-      height: '53vh'
-    }}>
-      <div className="container h-100">
-        <div className="row h-100 align-items-center">
-          <div className="col-12">
-            <div className="bradcumbContent">
-              <h2>About Us</h2>
+export const About = (props) => {
+  return (
+    <div>
+      <Header />
+      <div
+        className="mosh-breadcumb-area"
+        style={{
+          backgroundImage: `url(${breadcrumb})`,
+        }}
+      >
+        <div className="container h-100">
+          <div className="row h-100 align-items-center">
+            <div className="col-12">
+              <div className="bradcumbContent">
+                <h2>About Us</h2>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <section className="mosh-aboutUs-area">
-      <div className="mosh-about-us-content">
-        <div className="section-heading">
-          <h2>Quality Policy</h2>
+      <section className="mosh-aboutUs-area">
+        <div className="mosh-about-us-content">
+          <div className="section-heading">
+            <h2>Quality Policy</h2>
+          </div>
+          <p>
+            The quality policy of Standard Printers is to ensure customer
+            satisfaction by providing quality printing solutions for Offset and
+            Labels printing. We have identified the following core objectives in
+            our business:
+          </p>
+          <ul>
+            <li>
+              <p>
+                1. Achieve levels of service based on creating cost effective
+                printing solutions for clients.
+              </p>
+            </li>
+            <li>
+              <p>
+                2. Ensure profitability and growth through sound financial
+                management of the business.
+              </p>
+            </li>
+          </ul>
+          <p>
+            We are commited to continuous improvement in all aspects of our
+            business to improve the service to our customers and in all the
+            internal processes of the company.
+          </p>
+          <p>
+            ISO 9001: 2015 Quality Management system is the basis of our quality
+            management system and we are commited to meet the requirements.
+          </p>
         </div>
-        <p>The quality policy of Standard Printers is to ensure customer satisfaction by providing quality printing solutions for Offset and Labels printing. We have identified the following core objectives in our business:</p>
-        <ul>
-          <li><p>1. Achieve levels of service based on creating cost effective printing solutions for clients.</p></li>
-          <li><p>2. Ensure profitability and growth through sound financial management of the business.</p></li>
-        </ul>
-        <p>We are commited to continuous improvement in all aspects of our business to improve the service to our customers and in all the internal processes of the company.</p>
-        <p>ISO 9001: 2015 Quality Management system is the basis of our quality management system and we are commited to meet the requirements.</p>
-      </div>
-      <div className="mosh-about-us-thumb wow fadeInUp" data-wow-delay="0.5s">
-        <img src={certificate} alt="" />
-      </div>
-    </section>
-        
-    <Footer />
-  </div>
-}
+        <div className="mosh-about-us-thumb wow fadeInUp" data-wow-delay="0.5s">
+          <img src={certificate} alt="" />
+        </div>
+      </section>
+
+      <Footer />
+    </div>
+  );
+};
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,95 +1,128 @@
-import React from 'react';
+import React from "react";
 
-import breadcrumb from '../assets/img/core-img/breadcumb.png';
-import map from '../assets/img/core-img/map.png'
-import call from '../assets/img/core-img/call.png'
-import message from '../assets/img/core-img/message.png'
+import breadcrumb from "../assets/img/core-img/breadcumb.png";
+import map from "../assets/img/core-img/map.png";
+import call from "../assets/img/core-img/call.png";
+import message from "../assets/img/core-img/message.png";
 
-import { Header, Footer } from '../components'
+import { Header, Footer } from "../components";
 
-export const Contact = (props)=> {
-  return <div>
-    <Header />
-    <div class="mosh-breadcumb-area" style={{
-      backgroundImage: `url(${breadcrumb})`,
-      backgroundSize: '100vw',
-      backgroundRepeat: 'no-repeat',
-      height: '53vh'
-    }}>
-      <div class="container h-100">
-        <div class="row h-100 align-items-center">
-          <div class="col-12">
-            <div class="bradcumbContent">
-              <h2>Contact Us</h2>
+export const Contact = (props) => {
+  return (
+    <div>
+      <Header />
+      <div
+        class="mosh-breadcumb-area"
+        style={{
+          backgroundImage: `url(${breadcrumb})`,
+        }}
+      >
+        <div class="container h-100">
+          <div class="row h-100 align-items-center">
+            <div class="col-12">
+              <div class="bradcumbContent">
+                <h2>Contact Us</h2>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <section class="contact-area">
-      <div class="contact-left">
-        <form class="contact-form-area">
-          <h2>Get in touch</h2>
-            <input type="text" class="form-control" id="name" name="first_name" placeholder="Name" />
-            <input type="email" class="form-control" id="email" name="email" placeholder="E-mail" />
-            <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" />
-            <textarea name="message" class="form-control" id="message" 
-                      name="comments" cols="30" rows="10" placeholder="Message">
-            </textarea>
-            <button class="mosh-btn mt-50" type="submit">Send Message</button>
-        </form>
-      </div>
-      <div class="contact-right">
-        <div class="contact-information">
-          <h2>Contact</h2>
-          <div class="single-contact-info d-flex">
-            <div class="contact-icon mr-15">
+      <section class="contact-area">
+        <div class="contact-left">
+          <form class="contact-form-area">
+            <h2>Get in touch</h2>
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              name="first_name"
+              placeholder="Name"
+            />
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              name="email"
+              placeholder="E-mail"
+            />
+            <input
+              type="text"
+              class="form-control"
+              id="subject"
+              name="subject"
+              placeholder="Subject"
+            />
+            <textarea
+              name="message"
+              class="form-control"
+              id="message"
+              name="comments"
+              cols="30"
+              rows="10"
+              placeholder="Message"
+            ></textarea>
+            <button class="mosh-btn mt-50" type="submit">
+              Send Message
+            </button>
+          </form>
+        </div>
+        <div class="contact-right">
+          <div class="contact-information">
+            <h2>Contact</h2>
+            <div class="single-contact-info d-flex">
+              <div class="contact-icon mr-15">
                 <img src={map} alt="" class="contact-map-icon" />
+              </div>
+              <p>
+                <a href="https://g.page/sr-print-labels?share">
+                  Plot No. 137, Prasanthi Nagar, I.E Kukatpally Hyderabad,
+                  Telangana - 500072
+                </a>
+              </p>
             </div>
-            <p><a href="https://g.page/sr-print-labels?share">Plot No. 137, Prasanthi Nagar, I.E Kukatpally Hyderabad, Telangana - 500072</a></p>
-          </div>
-          <div class="single-contact-info d-flex">
-            <div class="contact-icon mr-15">
+            <div class="single-contact-info d-flex">
+              <div class="contact-icon mr-15">
                 <img src={call} alt="" />
+              </div>
+              <p>Main: 040 40208438</p>
             </div>
-            <p>Main: 040 40208438</p>
-          </div>
-          <div class="single-contact-info d-flex">
-            <div class="contact-icon mr-15">
+            <div class="single-contact-info d-flex">
+              <div class="contact-icon mr-15">
                 <img src={call} alt="" />
+              </div>
+              <p>Mobile: +91 9177664482</p>
             </div>
-            <p>Mobile: +91 9177664482</p>
-          </div>
-          <div class="single-contact-info d-flex">
-            <div class="contact-icon mr-15">
+            <div class="single-contact-info d-flex">
+              <div class="contact-icon mr-15">
                 <img src={message} alt="" />
+              </div>
+              <p>info@srprintlabels.com</p>
             </div>
-            <p>info@srprintlabels.com</p>
-          </div>
-          <div class="single-contact-info d-flex">
-            <div class="contact-icon mr-15">
+            <div class="single-contact-info d-flex">
+              <div class="contact-icon mr-15">
                 <img src={message} alt="" />
+              </div>
+              <p>srprintlabels@gmail.com</p>
             </div>
-            <p>srprintlabels@gmail.com</p>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    <div class="map-area">
-      <div class="map">
-        <iframe 
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15222.503822634968!2d78.424826!3d17.477608!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5c6f502818315e47!2sSR%20Print%20Labels!5e0!3m2!1sen!2sin!4v1576904753515!5m2!1sen!2sin"
-          frameborder="0" 
-          style={{border: '0', margin: '0'}}
-          width="100%"
-          height="500vh" 
-          allowfullscreen=""
-        ></iframe>
+      <div class="map-area">
+        <div class="map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15222.503822634968!2d78.424826!3d17.477608!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5c6f502818315e47!2sSR%20Print%20Labels!5e0!3m2!1sen!2sin!4v1576904753515!5m2!1sen!2sin"
+            frameborder="0"
+            style={{ border: "0", margin: "0" }}
+            width="100%"
+            height="500vh"
+            allowfullscreen=""
+          ></iframe>
+        </div>
       </div>
+
+      <Footer />
     </div>
-
-    <Footer />
-  </div>
-}
+  );
+};
